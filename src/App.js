f7d2a8378ad9f90f334b07eb73dc06c639737661
andrew <mrbondmustdie@gmail.com>
2021-01-15 10:08:30 +0800

    Added Material UI AppBar and styled

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-// import Navbar from "./components/Navbar";
+import Container from '@material-ui/core/Container';
+import NavBar from "./components/NavBar";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
     <Router>
       <div>
-       {/* <Navbar /> */}
-        {/*  <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer /> */}
+      <CssBaseline />
+        <NavBar />
+        <Container>
+          {/*  <Wrapper>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/discover" component={Discover} />
+            <Route exact path="/search" component={Search} />
+          </Wrapper>
+          <Footer /> */}
+        </Container>
       </div>
     </Router>
   );
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+
+const useStyles = makeStyles({
+  root: {
+    background: "#1C3664",
+    border: 0,
+    borderRadius: 0,
+    boxShadow: "0",
+    color: "white",
+    height: 110,
+    padding: "0 30px"
+  },
+  toolbar: {
+    minHeight: 80,
+    alignItems: 'flex-start',
+    paddingTop: 1,
+    paddingBottom: 2,
+  },
+  title: {
+    flexGrow: 1,
+    paddingLeft: 10,
+    marginTop: 60
+  },
+  icon: {
+    fontSize: 35,
+    height: 150
+  }
+});
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="static" className={classes.root}>
+        <Toolbar className={classes.toolbar}>         
+          <AccountCircleIcon className={classes.icon}/> 
+          <Typography className={classes.title} variant="h5" noWrap>
+          Employee Directory
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
