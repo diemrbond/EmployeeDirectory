f13f82170b5b27d6845c155bf56e44c64d9becb0
andrew <mrbondmustdie@gmail.com>
2021-01-15 08:21:51 +0800

    Initial React Setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div>
+       {/* <Navbar /> */}
+        {/*  <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/discover" component={Discover} />
+          <Route exact path="/search" component={Search} />
+        </Wrapper>
+        <Footer /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
